f0fa5ba43623ccb2043ba241873996722663792f
BVT.MI <2git@bovt.ru>
2020-11-28 17:55:22 +0300

    Empty spring boot form start.spring.io

---
+++ src/main/java/com/example/StudentTestingBvtApp/StudentTestingBvtAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentTestingBvtApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTestingBvtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTestingBvtAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StudentTestingBvtApp/StudentTestingBvtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentTestingBvtApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTestingBvtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
